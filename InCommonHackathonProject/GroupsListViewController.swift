1096babcd5a685593c91047626110f62d045545c
georgepazdralwork <georgpaz@aol.com>
2018-09-16 08:05:03 -0500

    blah blah

--- InCommonHackathonProject/GroupsListViewController.swift
+++ InCommonHackathonProject/GroupsListViewController.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+import Firebase
+import UIKit
+import Floaty
+
+class GroupsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.usersArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        <#code#>
+    }
+    
+     var usersArray = [ [String: Any] ]()
+    @IBOutlet var groupsList:UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+         let ref = Database.database().reference().child("groups").child("topics")
+        
+        ref.observeSingleEvent(of: .value, with: { snapshot in
+            
+            if ( snapshot.value is NSNull ) {
+                print("not found")
+            } else {
+                
+                
+                
+                for child in (snapshot.children) {
+                    
+                    let snap = child as! DataSnapshot //each child is a snapshot
+                    
+                    let dict = snap.value as? [String:AnyObject] // the value is a dict
+                    
+                    let key = snap.key
+                    self.usersArray.append(dict!)
+                    print(self.usersArray)
+                    self.keyArray.append(key)
+                }
+                self.tableView.reloadData()
+            }
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
